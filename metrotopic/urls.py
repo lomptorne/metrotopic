46ee5e2f96a7711e03a982ee7421f73f757680b6
Clement Zitouni <cazitouni@gmail.com>
2020-05-02 12:27:05 +0200

    jjJ

--- metrotopic/urls.py
+++ metrotopic/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('bc7uwcf/', admin.site.urls),
     path('', include('blog.urls')),
 ]
