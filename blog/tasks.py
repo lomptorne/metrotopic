d70ce56b0ec348bf1c1f5682d1c5c33e20fca8d5
Clément Zitouni <cazitouni@gmail.com>
2020-08-05 10:35:02 +0200

    t

--- blog/tasks.py
+++ blog/tasks.py
@@ -14,16 +14,22 @@ def collecteur(self, hashtag, imgNbr):
 
     # Set the function variable
     results =[]
+    headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+    "Accept-Language": "en-US,en;q=0.9,fr;q=0.8", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 Edg/84.0.522.52", 
+    "X-Amzn-Trace-Id": "Root=1-5f2a6dde-e2d2ef4d9b476c23a8c17d07"
+  }
     path = os.getcwd()
     url = "https://www.instagram.com/explore/tags/{}/?__a=1".format(hashtag)
-    page = requests.get(url)
+    page = requests.get(url, headers = headers)
     counter_progress = 0
     progress_recorder = ProgressRecorder(self)
     print(page)
     print(hashtag)
     print(imgNbr)
     print(page.content)
-    print(page.json())
+    
     try :
         jsonDump = page.json()['graphql']['hashtag']['edge_hashtag_to_media']['edges']
 
